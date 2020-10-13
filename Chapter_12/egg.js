b7d983ec71d3b92564d770b1a6dba54550af44b3
Nathan Anderson <nathandanielanderson@gmail.com>
2020-10-13 13:53:21 -0500

    finished chapter 12

--- Chapter_12/egg.js
+++ Chapter_12/egg.js
@@ -15,9 +15,8 @@ function parseExpression(program) {
 }
 
 function skipSpace(string) {
-    let first = string.search(/\S/);
-    if (first == -1) return "";
-    return string.slice(first);
+    let skippable = string.match(/^(\s|#.*)*/);
+    return string.slice(skippable[0].length);
 }
 
 function parseApply(expr, program) {
@@ -76,3 +75,165 @@ function evaluate(expr, scope) {
         }
     }
 }
+
+specialForms.if = (args, scope) => {
+    if (args.length != 3) {
+        throw new SyntaxError("Wrong number of args to if");
+    } else if (evaluate(args[0], scope) !== false) {
+        return evaluate(args[1], scope);
+    } else {
+        return evaluate(args[2], scope);
+    }
+};
+
+specialForms.while = (args, scope) => {
+    if(args.length != 2) {
+        throw new SyntaxError("Wrong number of args to while");
+    }
+    while (evaluate(args[0], scope) !== false) {
+        evaluate(args[1], scope);
+    }
+
+    //Since undefined does not exist in Egg, we return false,
+    //for lack of a meaningful result.
+    return false;
+};
+
+specialForms.do = (args, scope) => {
+    let value = false;
+    for (let arg of args) {
+        value = evaluate(arg, scope);
+    }
+    return value;
+};
+
+specialForms.define = (args, scope) => {
+    if (args.length != 2 || args[0].type != "word") {
+        throw new SyntaxError("Incorrect use of define");
+    }
+    let value = evaluate(args[1], scope);
+    scope[args[0].name] = value;
+    return value;
+};
+
+const topScope = Object.create(null);
+
+topScope.true = true;
+topScope.false = false;
+
+// let prog = parse(`if(true, false, true)`);
+// console.log(evaluate(prog, topScope));
+
+for (let op of ["+", "-", "*", "/", "==", "<", ">"]) {
+    topScope[op] = Function("a, b", `return a ${op} b;`);
+}
+
+topScope.print = value => {
+    console.log(value);
+    return value;
+};
+
+function run(program) {
+    return evaluate(parse(program), Object.create(topScope));
+}
+
+// run(`
+// do(define(total, 0),
+//    define(count, 1),
+//    while(<(count, 11),
+//         do(define(total, +(total, count)),
+//             define(count, +(count, 1)))),
+//             print(total))
+// `);
+
+specialForms.fun = (args, scope) => {
+    if (!args.length) {
+        throw new SyntaxError("Functions need a body");
+    }
+    let body = args[args.length - 1];
+    let params = args.slice(0, args.length - 1).map(expr => {
+        if(expr.type != "word") {
+            throw new SyntaxError("Parameter names must be words");
+        }
+        return expr.name;
+    });
+
+    return function() {
+        if (arguments.length != params.length) {
+            throw new TypeError("Wrong number of arguments");
+        }
+        let localScope = Object.create(scope);
+        for(let i = 0; i < arguments.length; i++) {
+            localScope[params[i]] = arguments[i];
+        }
+        return evaluate(body,localScope);
+    };
+};
+
+// run(`
+// do(define(plusOne, fun(a, +(a, 1))),
+// print(plusOne(10)))
+// `);
+
+// run(`
+// do(define(pow, fun(base, exp,
+//         if(==(exp, 0),
+//             1,
+//             *(base, pow(base, -(exp, 1)))))),
+//             print(pow(2,10)))
+// `);
+
+//Arrays
+topScope.array = (...values) => values;
+topScope.length = array => array.length;
+topScope.element = (array, i) => array[i];
+
+// run(`
+// do(define(sum, fun(array,
+//      do(define(i, 0),
+//         define(sum, 0),
+//         while(<(i, length(array)),
+//           do(define(sum, +(sum, element(array, i))),
+//              define(i, +(i, 1)))),
+//         sum))),
+//    print(sum(array(1, 2, 3))))
+// `);
+
+//Closure
+//When creating a function the scope is passed in  giving the local scope access to outer scope
+
+//Comments (modify skipSpace)
+// console.log(parse("# hello\nx"));
+// // → {type: "word", name: "x"}
+
+// console.log(parse("a # one\n   # two\n()"));
+// → {type: "apply",
+//    operator: {type: "word", name: "a"},
+//    args: []}
+
+//Fixing Scope
+specialForms.set = (args, env) => {
+    if (args.length != 2 || args[0].type != "word") {
+        throw new SyntaxError("Incorrect use of set");
+    }
+    let varName = args[0].name;
+    let value = evaluate(args[1], env);
+
+    for (let scope = env; scope; scope = Object.getPrototypeOf(scope)) {
+        if(Object.prototype.hasOwnProperty.call(scope, varName)) {
+            scope[varName] = value;
+            return value;
+        }
+    }
+    throw new ReferenceError(`Setting undefined vairable ${varName}`);
+};
+
+// run(`
+// do(define(x, 4),
+//    define(setx, fun(val, set(x, val))),
+//    setx(50),
+//    print(x))
+// `);
+// // → 50
+// run(`set(quux, true)`);
+// // → Some kind of ReferenceError
